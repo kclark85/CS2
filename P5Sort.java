e8f9e87daaeb885451fa243d207808055d9d87aa
kclark85 <kclark85@msudenver.edu>
2018-03-03 20:27:30 -0700

    update

--- P5Sort.java
+++ P5Sort.java
@@ -1,94 +1,117 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 
-public class P5Sort{
+public class P5Sort {
+    BufferedWriter writer;
+    DecimalFormat realFormatter;
     private final double MILSEC = 1000;
     private final double NANOSEC = 1000000000;
     long startWall, startCPU, stopWall, stopCPU;
+    double intBubbleWall, intBubbleCPU, stringBubbleWall, stringBubbleCPU, stringSelectionWall,
+            stringSelectionCPU, intSelectionWall, intSelectionCPU, stringJWall, stringJCPU, intJWall, intJCPU;
     Scanner userInput;
-    File inputFile;
+    File stringInputFile, intInputFile;
     ArrayList<String> stringList;
     ArrayList<Integer> intList;
+    Comparator<String> c = (o1, o2) -> {
+        if (o1.compareTo(o2) == 0) {
+            return 0;
+        }
+        if (o1.toUpperCase().compareTo(o2.toUpperCase()) > 0) {
+            return 1;
+        }
 
-    public P5Sort(){
+        if (o1.toUpperCase().compareTo(o2.toUpperCase()) < 0) {
+            return -1;
+        }
+
+        return o1.toUpperCase().compareTo(o2.toUpperCase());
+    };
+
+    Comparator<Integer> n = (o1, o2) -> {
+        if (o1 == o2) {
+            return 0;
+        }
+        if (o1 > o2) {
+            return 1;
+        }
+
+        if (o1 < o2) {
+            return -1;
+        }
+
+        return 0;
+    };
+
+    public P5Sort() {
         stringList = new ArrayList<>();
         intList = new ArrayList<>();
-    }
-
-    public void displayMenu(){ //allow user to chose between inputting file, choose different list sorts, and print data on sorting times
-
-    }
-
-    public void getFile(){
-        userInput = new Scanner(System.in);
-        System.out.println("Please enter the name of your file.");
-        String response = userInput.next();
-        if(inputFile != null){
-            System.out.println("WARNING: changing input file " + inputFile.getName() + " will clear all list elements." +
-                    " Do you want to continue?(Yes/No?)");
-            //check user responses
-            try {
-                if (response.equalsIgnoreCase("No")||response.equalsIgnoreCase("N")){
-                    return; // TODO: 2/28/2018 have this bring users back to the menu
-                }
-                if(!(response.equalsIgnoreCase("No")||response.equalsIgnoreCase("N")
-                        ||response.equalsIgnoreCase("Yes")||response.equalsIgnoreCase("Y"))){
-                    System.out.println("Invalid userInput, returning to menu...");
-                    displayMenu();
-                }
-
-            }
-            catch(Exception e){
-                System.out.println(e.getMessage());
-            }
-            //end check user userInput
-        }
         try {
-            inputFile = new File(response);
-        }
-        catch (NullPointerException e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println("Input file: " + inputFile.getName() +"\n");
-    }
-
-    public void loadLists(File in) {
-        try {
-            Scanner fileScanner = new Scanner(new BufferedReader( new FileReader(in)));
-            while(fileScanner.hasNext()) {
-                String sInput = fileScanner.next();
-                appendStringList(sInput);
-            }
-            clearNumbers();
-            fileScanner.close();
-            fileScanner = new Scanner(new BufferedReader( new FileReader(in)));
-            while(fileScanner.hasNextInt()){
-                appendIntList(fileScanner.nextInt());
-            }
-
+            writer = new BufferedWriter(new FileWriter("P5Output.txt"));
+            writer.write("Khamunetri Clark\n");
+            writer.newLine();
+            writer.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("Lists loaded, returning to menu...");
-        displayMenu();
     }
-    //ensures string list won't load with ints
-    void clearNumbers(){
-        if(stringList != null) {
-            for (int i = 0; i < stringList.size(); ) {
-                if (stringList.get(i).contains("0") || stringList.get(i).contains("1") ||
-                        stringList.get(i).contains("2") || stringList.get(i).contains("3") ||
-                        stringList.get(i).contains("4") || stringList.get(i).contains("5") ||
-                        stringList.get(i).contains("6") || stringList.get(i).contains("7") ||
-                        stringList.get(i).contains("8") || stringList.get(i).contains("9")) {
-                    stringList.remove(i);
+
+    public void getFile() {
+        stringList.clear();
+        intList.clear();
+
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter the name of your STRING input file.");
+            stringInputFile = new File(br.readLine());
+        System.out.println("Please enter the name of your INT input file.");
+            intInputFile = new File(br.readLine());
+                if(stringInputFile.exists() && intInputFile.exists()){
+                    System.out.println("String input file: " + stringInputFile.getName() + "\n"
+                                        + "Integer input file: " + intInputFile.getName() + "\n");
+                    userInput.close();
+                }else if(!stringInputFile.exists()){
+                    System.out.println("Input file not found, please try again.");
+                    getFile();
+                }else if(!intInputFile.exists()){
+                    System.out.println("Input file not found, please try again. ");
+                    getFile();
                 }
-                return;
+                userInput.close();
+            }catch (Exception e){
+
             }
+    }
+
+    public void loadLists() {
+        System.out.println("Loading lists...");
+        if (!intInputFile.exists()||!stringInputFile.exists()) {
+            System.out.println("Error reading input, please choose another file...");
+            getFile();
+        }
+        intList.clear();
+        stringList.clear();
+        try {
+            Scanner fileScanner = new Scanner(new BufferedReader(new FileReader(intInputFile)));
+            while (fileScanner.hasNextInt()) {
+                appendIntList(fileScanner.nextInt());
+            }
+            fileScanner.close();
+            fileScanner = new Scanner(new BufferedReader(new FileReader(stringInputFile)));
+            while (fileScanner.hasNext()) {
+                String sInput = fileScanner.next();
+                appendStringList(sInput);
+            }
+            fileScanner.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        if(intList.size() > 0 && stringList.size() > 0) {
+            System.out.println("Lists loaded!");
         }
     }
 
@@ -101,35 +124,271 @@ public class P5Sort{
 
     }
 
-    void swap(ArrayList<String> list, int m, int n){
-        String temp = list.get(m); //temp = index m of list
-        list.set(m, list.get(n)); //list(m) = list.get(n)
+    void stringSwap(ArrayList<String> list, int m, int n) {
+        String temp = list.get(m);
+        list.set(m, list.get(n));
         list.set(n, temp);
     }
 
-    void bubbleSort(ArrayList<String> list){
-        System.out.println("sorting...");
-        for(int i = 1; i < list.size() -1; i++){
-            for(int j = 0; j < list.size() -1; j++){
-                if(list.get(j).compareTo(list.get(j+1)) > 0){
-                    swap(list,j, j+1);
+    void intSwap(ArrayList<Integer> list, int m, int n) {
+        int temp = list.get(m);
+        list.set(m, list.get(n));
+        list.set(n, temp);
+    }
+
+    void intJavaSort(ArrayList<Integer> list){
+        System.out.println("Java sorting...");
+        startWall = System.currentTimeMillis();
+        startCPU = System.nanoTime();
+        list.sort(n);
+        stopWall = System.currentTimeMillis();
+        stopCPU = System.nanoTime();
+        intJWall = (stopWall - startWall) / MILSEC;
+        intJCPU = (stopCPU - startCPU) / NANOSEC;
+    }
+
+    void stringJavaSort(ArrayList<String> list) {
+        System.out.println("Java sorting...");
+        startWall = System.currentTimeMillis();
+        startCPU = System.nanoTime();
+        list.sort(c);
+        stopWall = System.currentTimeMillis();
+        stopCPU = System.nanoTime();
+        stringJWall = (stopWall - startWall) / MILSEC;
+        stringJCPU = (stopCPU - startCPU) / NANOSEC;
+
+    }
+
+    void stringSelectionSort(ArrayList<String> list) {
+        System.out.println("Selection sorting...");
+        startWall = System.currentTimeMillis();
+        startCPU = System.nanoTime();
+        for (int i = 0; i < list.size(); i++) {
+            int min = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j).toUpperCase().compareTo(list.get(min).toUpperCase()) < 0) {
+                    min = j;
+                }
+            }
+            stringSwap(list, i, min);
+        }
+        stopWall = System.currentTimeMillis();
+        stopCPU = System.nanoTime();
+        stringSelectionWall = (stopWall - startWall) / MILSEC;
+        stringSelectionCPU = (stopCPU - startCPU) / NANOSEC;
+    }
+
+    void intSelectionSort(ArrayList<Integer> list) {
+        System.out.println("Selection sorting...");
+        startWall = System.currentTimeMillis();
+        startCPU = System.nanoTime();
+        for (int i = 0; i < list.size(); i++) {
+            int min = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(min)) {
+                    min = j;
+                }
+            }
+            intSwap(list, i, min);
+        }
+        stopWall = System.currentTimeMillis();
+        stopCPU = System.nanoTime();
+        intSelectionWall = (stopWall - startWall) / MILSEC;
+        intSelectionCPU = (stopCPU - startCPU) / NANOSEC;
+    }
+
+    void stringBubbleSort(ArrayList<String> list) {
+        System.out.println("Bubble sorting...");
+        startWall = System.currentTimeMillis();
+        startCPU = System.nanoTime();
+        for (int i = 1; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1; j++) {
+                if (list.get(j).toUpperCase().compareTo(list.get(j + 1).toUpperCase()) > 0) {
+                    stringSwap(list, j, j + 1);
 
                 }
             }
         }
+        stopWall = System.currentTimeMillis();
+        stopCPU = System.nanoTime();
+        stringBubbleWall = (stopWall - startWall) / MILSEC;
+        stringBubbleCPU = (stopCPU - startCPU) / NANOSEC;
     }
 
+    void intBubbleSort(ArrayList<Integer> list) {
+        System.out.println("Bubble sorting...");
+        startWall = System.currentTimeMillis();
+        startCPU = System.nanoTime();
+        for (int i = 1; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    intSwap(list, j, j + 1);
 
+                }
+            }
+        }
+        stopWall = System.currentTimeMillis();
+        stopCPU = System.nanoTime();
+        intBubbleWall = (stopWall - startWall) / MILSEC;
+        intBubbleCPU = (stopCPU - startCPU) / NANOSEC;
+    }
+
+    void runSorts() {
+        if (intList.size() > 0) {
+            intBubbleSort(intList);
+            System.out.println("Bubble sort complete!");
+            //uncomment to see sorted lists
+            for (int i = 0; i < intList.size(); i++) {
+                System.out.println(intList.get(i));
+            }
+            loadLists(); //ensures each new load is random
+            intSelectionSort(intList);
+            System.out.println("Selection sort complete!");
+            //uncomment to see sorted lists
+            for (int i = 0; i < intList.size(); i++) {
+                System.out.println(intList.get(i));
+            }
+            loadLists();
+            intJavaSort(intList);
+            System.out.println("Java sort complete!");
+            //uncomment to see sorted lists
+            for (int i = 0; i < intList.size(); i++) {
+                System.out.println(intList.get(i));
+
+            }
+        }else if(intList.size() == 0){
+            System.out.println("Lists are empty. Check input file");
+            getFile();
+            }
+//        }   else if (stringList.size() > 0 && intList.size() == 0) {
+//            stringBubbleSort(stringList);
+//            System.out.println("Bubble sort complete!");
+//            //uncomment to see sorted lists
+////            for (int i = 0; i < stringList.size(); i++) {
+////                System.out.println(intList.get(i));
+////            }
+//            loadLists(inputFile);
+//            stringSelectionSort(stringList);
+//            System.out.println("Selection sort complete!");
+//            //uncomment to see sorted lists
+////            for (int i = 0; i < stringList.size(); i++) {
+////                System.out.println(stringList.get(i));
+////            }
+//            loadLists(inputFile);
+//            stringJavaSort(stringList);
+//            System.out.println("Java Sort complete!");
+//            //uncomment to see sorted lists
+////            for (int i = 0; i < stringList.size(); i++) {
+////                System.out.println(stringList.get(i));
+////            }
+//
+//        }
+
+    }
+
+    void writeSorts() {
+        if (stringInputFile == null || !stringInputFile.exists()) {
+            System.out.println("Invalid file, please choose another");
+            getFile();
+        }
+        if (stringList.size() == 0) {
+            System.out.println("Lists are empty. Check input file");
+            getFile(); // if lists are empty, it is because they could not be read in from file
+        }
+//            writeIntSorts();
+            writeStringSorts();
+    }
+
+    void writeIntSorts(){
+        realFormatter = new DecimalFormat("0.0000");
+        DecimalFormat sizeFormat = new DecimalFormat("0,000");
+        System.out.println("Writing to file...");
+        try {
+            writer = new BufferedWriter(new FileWriter("P5Output.txt", true));
+            writer.write("INTEGER SORT");
+            writer.newLine();
+            writer.write("There are " + sizeFormat.format(intList.size()) + " numbers in the list.");
+            writer.newLine();
+            writer.write("Bubble Sort:      " + this.realFormatter.format(intBubbleCPU) +    " seconds CPU time    " + this.realFormatter.format(intBubbleWall) + " seconds wall clock ");
+            writer.newLine();
+            writer.write("Selection Sort:   " + this.realFormatter.format(intSelectionCPU) + " seconds CPU time    " + this.realFormatter.format(intSelectionWall) + " seconds wall clock ");
+            writer.newLine();
+            writer.write("Java sort:        " + this.realFormatter.format(intJCPU) +         " seconds CPU time    " + this.realFormatter.format(intJWall) + " seconds wall clock ");
+            writer.newLine();
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Writing complete!");
+    }
+
+    void writeStringSorts(){
+        realFormatter = new DecimalFormat("0.0000");
+        DecimalFormat sizeFormat = new DecimalFormat("0,000");
+        System.out.println("Sorting string inputs and writing to file...");
+        stringBubbleSort(stringList);
+        System.out.println("Bubble sort complete!");
+        loadLists();
+        stringSelectionSort(stringList);
+        System.out.println("Selection sort complete!");
+        loadLists();
+        stringJavaSort(stringList);
+        System.out.println("Java sort complete!");
+        try {
+            writer = new BufferedWriter(new FileWriter("P5Output.txt", true));
+            writer.write("STRING SORT");
+            writer.newLine();
+            writer.write("There are " + sizeFormat.format(stringList.size()) + " words in the list.");
+            writer.newLine();
+            writer.write("Bubble Sort:      " + this.realFormatter.format(stringBubbleCPU) +    " seconds CPU time    " + this.realFormatter.format(stringBubbleWall) + " seconds wall clock ");
+            writer.newLine();
+            writer.write("Selection Sort:   " + this.realFormatter.format(stringSelectionCPU) + " seconds CPU time    " + this.realFormatter.format(stringSelectionWall) + " seconds wall clock ");
+            writer.newLine();
+            writer.write("Java sort:        " + this.realFormatter.format(stringJCPU) +         " seconds CPU time    " + this.realFormatter.format(stringJWall) + " seconds wall clock ");
+            writer.newLine();
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Writing complete!");
+    }
+
+    void printIntSorts(){
+        realFormatter = new DecimalFormat("0.0000");
+        System.out.println();
+        System.out.println("ELAPSED TIME:");
+        System.out.println("Integer Bubble Sort:      " + this.realFormatter.format(intBubbleCPU) +    " seconds CPU time    " + this.realFormatter.format(intBubbleWall) + " seconds wall clock ");
+        System.out.println();
+        System.out.println("Integer Selection Sort:   " + this.realFormatter.format(intSelectionCPU) + " seconds CPU time    " + this.realFormatter.format(intSelectionWall) + " seconds wall clock ");
+        System.out.println();
+        System.out.println("Integer Java Sort:        " + this.realFormatter.format(intJCPU) +         " seconds CPU time    " + this.realFormatter.format(intJWall) + " seconds wall clock ");
+        System.out.println();
+    }
     public static void main(String[] args) {
         P5Sort run = new P5Sort();
-        run.getFile();
-        run.loadLists(run.inputFile);
-        System.out.println(run.intList.size() + " " + run.stringList.size());
-        run.bubbleSort(run.stringList);
-        for(int i = 0; i < run.stringList.size(); i++) {
-            System.out.println(run.stringList.get(i));
-        }
+        System.out.println("P5 Sorts: given an input file, test the performance of a number of sorts and save the output.");
+        try {
 
+                run.getFile();
+                run.loadLists();
+                if(run.intList.size() == 0 || run.stringList.size() == 0){
+                    while(run.intList.size() == 0 || run.stringList.size() == 0) {
+                        System.out.println("Loading failed, check your input file.");
+                        run.intInputFile = null;
+                        run.stringInputFile = null;
+                        run.getFile();
+                        run.loadLists();
+                    }
+                }
+                run.runSorts();
+                run.printIntSorts();
+                run.writeSorts();
+                System.out.println("Exiting program. Check File P5Output.txt for results.");
+
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
 
